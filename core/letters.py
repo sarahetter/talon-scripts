cafee64b76dc39ac6e98a7c90fc014cc94a08b75
Sarah Etter <sarah@sarahetter.com>
2026-05-04 08:25:59 -0400

    change letters

---
+++ core/letters.py
@@ -0,0 +1,32 @@
+from talon import Context
+
+ctx = Context()
+
+ctx.lists["user.letter"] = {
+    "air": "a",
+    "bat": "b",
+    "cap": "c",
+    "drum": "d",
+    "each": "e",
+    "fine": "f",
+    "gust": "g",
+    "harp": "h",
+    "ink": "i",
+    "job": "j",
+    "kid": "k",
+    "look": "l",
+    "made": "m",
+    "near": "n",
+    "odd": "o",
+    "pit": "p",
+    "quench": "q",
+    "red": "r",
+    "sun": "s",
+    "trap": "t",
+    "urge": "u",
+    "vest": "v",
+    "whale": "w",
+    "plex": "x",
+    "yank": "y",
+    "zip": "z",
+}
